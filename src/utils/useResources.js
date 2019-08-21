dcc5192d2f2edae31b583289e822f7ea2b54ebf0
Andrey Ramirez <ramirez.jag00@gmail.com>
2019-08-21 22:06:31 +0800

    Move resource url to a constant

--- src/utils/useResources.js
+++ src/utils/useResources.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 const useResources = (resource) => {
     const [resources, setResources] = useState([]);
-  
+    const url = (resource) => `https://jsonplaceholder.typicode.com/${resource}`;
     useEffect(
       () => {
         (async resource => {
-          const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+          const response = await axios.get(url(resource));
           setResources(response.data);
         })(resource)
       }, [resource]);
